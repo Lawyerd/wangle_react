7e396c5412bbb31b497c1f9cc5754929be9c3d20
Lawyerd <jun12376@gmail.com>
2021-06-12 00:57:42 +0900

    Add @toast-editor in BoardWriter

--- my-app/src/routes/Board/BoardWrite.js
+++ my-app/src/routes/Board/BoardWrite.js
@@ -2,22 +2,32 @@ import React, { useState, useEffect } from "react";
 
 import "codemirror/lib/codemirror.css";
 import "@toast-ui/editor/dist/toastui-editor.css";
-import { Editor } from "@toast-ui/react-editor";
+
+import { Editor, Viewer } from "@toast-ui/react-editor";
 import { Card, Button, ButtonGroup } from "react-bootstrap";
 import "tui-pagination/dist/tui-pagination.css";
 import base_url from "../../data/base_url.js";
 
 function BoardWrite() {
+  function onClick(e) {
+    // console.log(e.target.parentElement);
+    var content = e.target.parentElement.childNodes[0].getMarkdown();
+    console.log(content);
+  }
   return (
     <Card>
       <Card.Body>
-        <Editor
-          initialValue="hello react editor world!"
-          previewStyle="vertical"
-          height="600px"
-          initialEditType="markdown"
-          useCommandShortcut={true}
-        />
+        <div>
+          <Editor
+            initialValue="hello react editor world!"
+            previewStyle="tab"
+            initialEditType="markdown"
+            useCommandShortcut={true}
+          />
+          <Viewer initialValue="content"></Viewer>
+          {/* <Button onClick={onClick}>hi</Button> */}
+        </div>
+        {/* https://1nnovator.tistory.com/56 */}
       </Card.Body>
     </Card>
   );
--- my-app/src/routes/Index.js
+++ my-app/src/routes/Index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../css/About.css";
 // import newyork from "../img/newyork.jpg";
-import Clock from "react-live-clock";
+// import Clock from "react-live-clock";
 
 // import Clock from "../components/Clock.js";
 
@@ -9,12 +9,12 @@ class Index extends React.Component {
   render() {
     return (
       <div className="clock_wrap">
-        <Clock
+        {/* <Clock
           format={"HH:mm:ss"}
           ticking={true}
           timezone={"Asia/Seoul"}
           interval={1000}
-        />
+        /> */}
       </div>
     );
   }
